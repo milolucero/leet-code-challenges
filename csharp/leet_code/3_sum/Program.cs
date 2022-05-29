59dad252979f2e8b68c61862b7d4e3113e57496c
milolucero <camiloluceroe@gmail.com>
2022-05-28 21:55:56 -0500

    Worked on solving "Letter combinations of a phone number" and "Three sum".

---
+++ csharp/leet_code/3_sum/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * LeetCode Challenge
+ * Author: Camilo Lucero
+ * Challenge name: 3Sum
+ * Description: Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+ * URL: https://leetcode.com/problems/3sum/
+ * Language: C#
+ * Status: In progress
+ */
+
+IList<IList<int>> ThreeSum(int[] nums)
+{
+    Array.Sort(nums);
+
+    List<int[]> res = new List<int[]>();
+
+    for (int i = 0; i < nums.Length - 2; i++)
+    {
+        if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+        {
+            int lowIndex = i + 1,
+                highIndex = nums.Length - 1,
+                sum = 0 - nums[i];
+
+            while (lowIndex < highIndex)
+            {
+                if (nums[lowIndex] + nums[highIndex] == sum)
+                {
+                    res.Add(new int[] { nums[i], nums[lowIndex], nums[highIndex] });
+
+                    while (lowIndex < highIndex && nums[lowIndex] == nums[lowIndex + 1])
+                    {
+                        lowIndex++;
+                    }
+
+                    while (lowIndex < highIndex && nums[highIndex] == nums[highIndex - 1])
+                    {
+                        highIndex--;
+                    }
+
+                    lowIndex++;
+                    highIndex++;
+                }
+                else if (nums[lowIndex] + nums[highIndex] < sum)
+                {
+                    lowIndex++;
+                } 
+                else
+                {
+                    highIndex--;
+                }
+            }
+        }
+    }
+
+    return (IList<IList<int>>)res;
+}
+
+// Test cases
+//List<(int[], IList<IList<int>>)> testCases = new List<(int[], IList<IList<int>>)>
+//{
+//    (new int[] {-1,0,1,2,-1,-4}, new List<IList<int>> { new int[] { -1, -1, 2 }, new int[] { -1, 0, 1 } })
+//};
+
+ThreeSum(new int[] { -1, 0, 1, 2, -1, -4 });
+
+// Testing
---
+++ csharp/leet_code/letter_combinations_of_a_phone_number/Program.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * LeetCode Challenge
+ * Author: Camilo Lucero
+ * Challenge name: Letter Combinations of a Phone Number
+ * Description: Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+ * URL: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+ * Language: C#
+ * Status: Solved
+ */
+
+IList<string> LetterCombinations(string digits)
+{
+    // Exit point
+    if (String.IsNullOrEmpty(digits)) return new List<string>() {};
+
+    IList<string> result = new List<string>();
+
+    string currentLetters = DigitToLetters(digits[0]);
+
+    for (int i = 0; i < currentLetters.Length; i++)
+    {
+        result = LetterCombinations(digits.Substring(i + 1));
+    }
+
+    //// Use recursion
+    //for (int i = 0; i < digits.Length; i++)
+    //{
+    //    foreach (string letters in LetterCombinations(digits.Substring(i + 1)))
+    //    {
+    //        string currentLetters = DigitToLetters(digits[0]);
+
+    //        foreach (char letter in currentLetters)
+    //        {
+    //            result.Add(letter.ToString());
+    //        }
+    //    }
+    //}
+
+    return result;
+}
+
+// Returns the letters represented on a phone by the given digit.
+string DigitToLetters(char digit)
+{
+    List<(char numberString, string letters)> dictionary = new List<(char numberString, string letters)>
+    {
+        ('2', "abc"),
+        ('3', "def"),
+        ('4', "ghi"),
+        ('5', "jkl"),
+        ('6', "mno"),
+        ('7', "pqrs"),
+        ('8', "tuv"),
+        ('9', "wzyz"),
+    };
+
+    for (int i = 0; i < dictionary.Count; i++)
+    {
+        if (digit == dictionary[i].numberString)
+        {
+            return dictionary[i].letters;
+        }
+    }
+
+    throw new Exception("No digit matched the given input.");
+}
+
+
+// Test cases
+List<(string, List<string>)> testCases = new List<(string, List<string>)>
+{
+    ("2", new List<string> {"a","b","c"}),
+    //("23", new List<string> {"ad","ae","af","bd","be","bf","cd","ce","cf"}),
+    //("", new List<string> {}),
+};
+
+// Testing
+foreach ((string numberString, IList<string> expected) in testCases)
+{
+    IList<string> actual = LetterCombinations(numberString);
+
+    Console.WriteLine($"\nTesting: {numberString}\nExpected: {String.Join(", ", expected)}\nActual: {String.Join(", ", actual)}");
+
+    Console.WriteLine(String.Join(", ", actual) == String.Join(", ", expected) ? "PASSED" : "FAILED");
+}
